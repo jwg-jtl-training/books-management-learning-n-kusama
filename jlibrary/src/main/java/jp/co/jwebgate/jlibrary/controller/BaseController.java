1977db6b9c69c2ecb5a8c5dea2f95a30ae576bf4
m-kinoshita <m-kinoshita@jwebgate.co.jp>
2023-09-15 10:44:16 +0900

    BaseController.java新規

---
+++ jlibrary/src/main/java/jp/co/jwebgate/jlibrary/controller/BaseController.java
@@ -0,0 +1,24 @@
+package jp.co.jwebgate.jlibrary.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+
+@Controller
+public class BaseController {
+    
+    public List<String> getFieldErrorMsgList(BindingResult bindingResult) {
+        
+        List<String> errorMsgList = new ArrayList<String>();
+        
+        if(bindingResult != null && bindingResult.hasFieldErrors()) {
+            errorMsgList = bindingResult.getFieldErrors().
+                    stream().map(e->e.getDefaultMessage()).collect(Collectors.toList());
+        }
+        
+        return errorMsgList;
+    }
+}
