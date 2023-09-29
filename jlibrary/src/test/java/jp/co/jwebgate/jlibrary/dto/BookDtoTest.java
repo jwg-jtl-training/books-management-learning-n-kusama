fc445d3dbd9078b0bebf97ab6cd1cda7d07c1e67
k-sato <k-sato@jtechlab.co.jp>
2023-09-29 10:35:07 +0900

    BookDtoTestにjavadocを追記しました。

--- jlibrary/src/test/java/jp/co/jwebgate/jlibrary/dto/BookDtoTest.java
+++ jlibrary/src/test/java/jp/co/jwebgate/jlibrary/dto/BookDtoTest.java
@@ -11,12 +11,21 @@ import org.springframework.boot.test.context.SpringBootTest;
 import jp.co.jwebgate.jlibrary.dbflute.exentity.Book;
 import jp.co.jwebgate.jlibrary.dbflute.exentity.Genre;
 
+
+/**
+ * BookDtoクラスのテスト
+ */
 @SpringBootTest
 class BookDtoTest {
 
 	@Mock
 	OptionalThingExceptionThrower thrower;
 	
+	
+	/**
+	 * BookDtoメソッドのテスト
+	 * ISBN10がnullでない場合、Isbn10の値を取得
+	 */
 	@Test
 	void test1() {
 		
@@ -41,32 +50,12 @@ class BookDtoTest {
 		assertEquals(dto.getGenreId(), book.getGenreId());	
 	}
 	
+	/**
+	 * BookDtoメソッドのテスト
+	 * ISBN10がnullの場合、Isbn13の値を取得
+	 */
 	@Test
 	void test2(){
-		
-		Book book = new Book();
-		book.setTitle("title");
-		book.setAuthor("author");
-		book.setPublisher("publisher");
-		book.setIsbn13("isbn13");
-		book.setGenreId(1);
-		Genre genre = new Genre();
-		
-		OptionalEntity<Genre> opt = new OptionalEntity<Genre>(genre, thrower);
-		book.setGenre(opt);
-		
-		BookDto dto = new BookDto(book);
-		
-		
-		assertEquals(dto.getTitle(), book.getTitle());
-		assertEquals(dto.getAuthor(), book.getAuthor());
-		assertEquals(dto.getPublisher(), book.getPublisher());
-		assertEquals(dto.getIsbn(), book.getIsbn13());
-		assertEquals(dto.getGenreId(), book.getGenreId());	
-	}
-	
-	@Test
-	void test3(){
 		
 		Book book = new Book();
 		book.setTitle("title");
@@ -80,7 +69,6 @@ class BookDtoTest {
 		book.setGenre(opt);
 		
 		BookDto dto = new BookDto(book);
-		
 		
 		assertEquals(dto.getTitle(), book.getTitle());
 		assertEquals(dto.getAuthor(), book.getAuthor());
