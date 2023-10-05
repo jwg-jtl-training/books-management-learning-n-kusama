eef5d4af9336023fbd699df4c5dfa70eb83059ba
kusadokoro kanon <k-kusadokoro@jtechlab.co.jp>
2023-10-05 16:55:38 +0900

    項目shelfNumberの追加

--- jlibrary/src/main/java/jp/co/jwebgate/jlibrary/dto/BookDto.java
+++ jlibrary/src/main/java/jp/co/jwebgate/jlibrary/dto/BookDto.java
@@ -7,23 +7,26 @@ import lombok.Data;
 public class BookDto {
 
 	private String title;
-	
+
 	private String author;
-	
+
 	private String publisher;
-	
+
 	private String isbn;
-	
+
 	private Integer genreId;
-	
+
+	private String shelfNumber;
+
 	private GenreDto genreDto;
-	
-	public BookDto(Book book){
+
+	public BookDto(Book book) {
 		this.title = book.getTitle();
 		this.author = book.getAuthor();
 		this.publisher = book.getPublisher();
 		this.isbn = book.getIsbn10() != null ? book.getIsbn10() : book.getIsbn13();
 		this.genreId = book.getGenreId();
+		this.shelfNumber = book.getShelfNumber();
 		this.genreDto = new GenreDto(book.getGenre().get());
 	}
 }
