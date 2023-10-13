c4998f416837ad6009325ff496eb46104e012a26
y-fukushima <y-fukushima@jwebgate.co.jp>
2023-10-13 12:41:33 +0900

    【Phase1】エラーメッセージの修正

--- jlibrary/src/main/java/jp/co/jwebgate/jlibrary/form/RegisterGeneralUserForm.java
+++ jlibrary/src/main/java/jp/co/jwebgate/jlibrary/form/RegisterGeneralUserForm.java
@@ -14,35 +14,35 @@ public class RegisterGeneralUserForm {
 	/**
 	* フリガナ
 	*/
-	@Size(max = 50, message = "フリガナは50字以内で入力してください")
-	@Pattern(regexp = "^[\\u30a0-\\u30ff]+$", message = "フリガナを全角カナで入力してください")
+	@Size(max = 50, message = "フリガナは50文字以下で入力してください。")
+	@Pattern(regexp = "^[\\u30a0-\\u30ff]+$", message = "フリガナを全角カナで入力してください。")
 	private String nameKana;
 	
 	/**
 	* 氏名
 	*/
-	@NotEmpty(message = "氏名を入力してください")
-	@Size(max = 25, message = "氏名は25字以内で入力してください")
+	@NotEmpty(message = "氏名を入力してください。")
+	@Size(max = 25, message = "氏名は25文字以下で入力してください。")
 	private String name;
 	
 	/**
 	* 生年月日
 	*/
-	@NotEmpty(message = "生年月日を入力してください")
+	@NotEmpty(message = "生年月日を入力してください。")
 	private String birthday;
 	
 	/**
 	* 電話番号
 	*/
-	@Size(min = 10, max = 11, message = "電話番号を10桁から11桁で入力してください")
-	@Pattern(regexp = "^[0-9]+$", message = "電話番号を半角数字で入力してください")
+	@Size(min = 10, max = 11, message = "電話番号を10桁もしくは11桁で入力してください。")
+	@Pattern(regexp = "^[0-9]+$", message = "電話番号を半角数字で入力してください。")
 	private String phoneNumber;
 	
 	/**
 	* 住所
 	*/
-	@NotEmpty(message = "住所を入力してください")
-	@Size(max = 255, message = "住所は255桁以内で入力してください")
+	@NotEmpty(message = "住所を入力してください。")
+	@Size(max = 255, message = "住所は255文字以下で入力してください。")
 	private String address;
 	
 	public RegisterGeneralUserForm() {}
