e5da6af1da37e591965b450741564f131bb02cec
y-fukushima <y-fukushima@jwebgate.co.jp>
2023-10-17 11:51:06 +0900

    【studyPhase1】図書検索画面で著者名でも検索できるように検索項目を追加

--- jlibrary/src/main/java/jp/co/jwebgate/jlibrary/repository/BookRepository.java
+++ jlibrary/src/main/java/jp/co/jwebgate/jlibrary/repository/BookRepository.java
@@ -18,48 +18,46 @@ import jp.co.jwebgate.jlibrary.dbflute.exentity.Book;
  */
 @Repository
 public class BookRepository {
-	
+
 	@Autowired
 	private BookBhv bookBhv;
-	
+
 	/**
-     * タイトルから図書情報リストの取得
-     *
-     * @param title タイトル
-     * @return 図書情報リスト
-     */
-	public List<Book> selectBookByTitle(String title){
-		return bookBhv.selectList(cb->{
-			if(!StringUtils.isNullOrEmpty(title )) {
-				cb.orScopeQuery(orCB -> {
-				orCB.query().setTitle_LikeSearch(title, op -> op.likeContain());
-				orCB.query().setAuthor_LikeSearch(title, op -> op.likeContain());
-			});}
+	 * タイトルから図書情報リストの取得
+	 *
+	 * @param title タイトル
+	 * @return 図書情報リスト
+	 */
+	public List<Book> selectBookByTitle(String title) {
+		return bookBhv.selectList(cb -> {
+			if (!StringUtils.isNullOrEmpty(title)) {
+				cb.query().setTitle_LikeSearch(title, op -> op.likeContain());
+			}
 			cb.setupSelect_Genre();
 		}).getSelectedList();
 	}
-	
+
 	/**
-     * 図書情報の挿入
-     *
-     * @param book 図書情報
-     */
+	 * 図書情報の挿入
+	 *
+	 * @param book 図書情報
+	 */
 	public void insertBook(Book book) {
 		bookBhv.insert(book);
 	}
-	
+
 	/**
-     * 管理番号の最大値の取得
-     *
-     * @return 管理番号の最大値
-     */
+	 * 管理番号の最大値の取得
+	 *
+	 * @return 管理番号の最大値
+	 */
 	public String selectMaxControlNumber() {
-		OptionalScalar<String> optSclr = bookBhv.selectScalar(String.class).max(cb->{
+		OptionalScalar<String> optSclr = bookBhv.selectScalar(String.class).max(cb -> {
 			cb.specify().columnControlNumber();
 		});
-		if(optSclr.isPresent()) {
+		if (optSclr.isPresent()) {
 			return optSclr.get();
-		}else {
+		} else {
 			return "0000000000";
 		}
 	}
