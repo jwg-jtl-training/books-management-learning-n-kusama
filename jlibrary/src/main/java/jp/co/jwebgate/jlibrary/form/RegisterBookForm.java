aaeb54752dd7983c1030a67650c6d54b8807b494
y-fukushima <y-fukushima@jwebgate.co.jp>
2023-10-17 17:18:28 +0900

    【studyPhase1】図書登録画面にバリデーションを追加する

--- jlibrary/src/main/java/jp/co/jwebgate/jlibrary/form/RegisterBookForm.java
+++ jlibrary/src/main/java/jp/co/jwebgate/jlibrary/form/RegisterBookForm.java
@@ -1,10 +1,7 @@
 package jp.co.jwebgate.jlibrary.form;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
 
-import jp.co.jwebgate.jlibrary.consts.RegularExpressionConsts;
 import lombok.Data;
 /**
  * 図書登録フォーム
@@ -15,27 +12,22 @@ public class RegisterBookForm {
 	/**
 	 * タイトル
 	 */
-	@NotBlank
 	private String title;
 	/**
 	 * 著者名
 	 */
-	@NotBlank
 	private String author;
 	/**
 	 * 出版社
 	 */
-	@NotBlank
 	private String publisher;
 	/**
 	 * ISBN-10
 	 */
-	@Size(max = 10, message = "{sizeMax}")
 	private String isbn10;
 	/**
 	 * ISBN-13
 	 */
-	@Size(max = 13, message = "{sizeMax}")
 	private String isbn13;
 	/**
 	 * ジャンルID
@@ -45,8 +37,6 @@ public class RegisterBookForm {
 	/**
 	 * 棚番号
 	 */
-	@Size(max = 3, message = "{sizeMax}")
-	@Pattern(regexp = RegularExpressionConsts.halfAlphabetNumber, message = "{patternHanEisuzi}")
 	private String shelfNumber;
 	/**
 	 * 管理番号
