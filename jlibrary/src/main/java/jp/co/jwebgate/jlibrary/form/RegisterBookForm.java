453d743bcc4be503ab8a0d18c165e6842d5bc0b1
k-kusadokoro <k-kusadokoro@jtechlab.co.jp>
2023-10-19 12:36:22 +0900

    課題AN002の修正

--- jlibrary/src/main/java/jp/co/jwebgate/jlibrary/form/RegisterBookForm.java
+++ jlibrary/src/main/java/jp/co/jwebgate/jlibrary/form/RegisterBookForm.java
@@ -1,7 +1,11 @@
 package jp.co.jwebgate.jlibrary.form;
+import java.util.Objects;
+
+import javax.validation.constraints.AssertTrue;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+import jp.co.jwebgate.jlibrary.consts.RegularExpressionConsts;
 import lombok.Data;
 /**
  * 図書登録フォーム
@@ -43,4 +47,89 @@ public class RegisterBookForm {
 	 */
 	@NotBlank
 	private String controlNumber;
+	
+	/**
+	 * ISBN10,ISBN13のどちらか片方が入力されているかの確認
+	 * @return 判定結果
+	 */
+
+	@AssertTrue
+	public boolean isCheckISBN() {
+		if (isbn10 == null || isbn13 == null) {
+			return true;
+		}
+
+		if (isbn10.length() >= 1 && isbn13.length() >= 1) {
+			return false;
+		}
+
+		if (isbn10.length() >= 1 || isbn13.length() >= 1) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * ISBN10の値が10桁であるかの確認
+	 * @return 判定結果
+	 */
+	@AssertTrue
+	public boolean isCheckISBN10() {
+		if (isbn10 == null && isbn13 == null) {
+			return true;
+		}
+
+		if (isbn10 != null && isbn10.length() >= 1) {
+			if (isbn10.length() != 10) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * ISBN13の値が13桁であるかの確認
+	 * @return 判定結果
+	 */
+	@AssertTrue
+	public boolean isCheckISBN13() {
+		if (isbn10 == null && isbn13 == null) {
+			return true;
+		}
+
+		if (isbn13 != null && isbn13.length() >= 1) {
+			if (isbn13.length() != 13) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * 棚番号の値が3桁であるかの確認
+	 * @return 判定結果
+	 */
+	@AssertTrue
+	public boolean isCheckShelfNumberSize() {
+		if (shelfNumber == null || shelfNumber.isEmpty()) {
+			return true;
+		}
+		return Objects.equals(shelfNumber.length(), 3);
+	}
+
+	/**
+	 * 棚番号の値が半角英数字であるかの確認
+	 * @return 判定結果
+	 */
+	@AssertTrue
+	public boolean isCheckShelfNumber() {
+		if (shelfNumber == null || shelfNumber.isEmpty()) {
+			return true;
+		}
+
+		if (shelfNumber.matches(RegularExpressionConsts.halfAlphabetNumber)) {
+			return true;
+		}
+		return false;
+	}
 }
